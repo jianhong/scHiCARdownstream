0304779517a2a81d98b30e4e458067561381d1a5
Jianhong Ou <jianhong.ou@gmail.com>
2026-05-11 11:58:31 -0400

    update callPeaks.

--- R/callPeaks.R
+++ R/callPeaks.R
@@ -2,6 +2,7 @@
 #' @param proj An ArchRProject object. The output of \link{processArchR}.
 #' @param groupBy The groups used to call peaks by function \link[ArchR:addReproduciblePeakSet]{addReproduciblePeakSet}.
 #' @param pathToMacs2 The macs2 path.
+#' @param ... Other parameters used by \link[ArchR:addReproduciblePeakSet]{addReproduciblePeakSet}.
 #' @param reducedDims The reductions used to link the gene and peaks.
 #' @param verbose Print message or not.
 #' @return An ArchRProject object.
@@ -14,11 +15,12 @@ callPeaks <- function(
     proj,
     groupBy='Clusters_Combined',
     pathToMacs2=findMacs2(),
+    ...,
     reducedDims='LSI_Combined',
     verbose=TRUE){
   stopifnot(is(proj, 'ArchRProject'))
   proj <- addGroupCoverages(ArchRProj = proj, groupBy = groupBy, verbose = FALSE)
-  proj <- addReproduciblePeakSet(ArchRProj = proj, groupBy = groupBy, pathToMacs2 = pathToMacs2)
+  proj <- addReproduciblePeakSet(ArchRProj = proj, groupBy = groupBy, pathToMacs2 = pathToMacs2, ...)
   proj <- addPeakMatrix(ArchRProj = proj)
   ## could we use hic links?
   proj <- addPeak2GeneLinks(ArchRProj = proj,
